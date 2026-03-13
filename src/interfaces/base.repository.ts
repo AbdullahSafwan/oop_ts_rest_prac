eaf8a4c4d9e7e26de7302f2d37f6a231a43f9795
AbdullahSafwan <abdullahsafwan768@gmail.com>
2026-03-14 04:27:08 +0500

    db connection

--- src/interfaces/base.repository.ts
+++ src/interfaces/base.repository.ts
@@ -1,8 +1,8 @@
-export interface BaseRepository<T> {
+export interface BaseRepository<T, F = Partial<T>> {
   create(data: T): Promise<T>;
   findAll(): Promise<T[]>;
   findById(id: string): Promise<T | null>;
   update(id: string, data: T): Promise<T | null>;
   delete(id: string): Promise<T | null>;
-  findAllPaginatedWithFilter(filter: any, page: number, limit: number): Promise<T[]>;
+  findAllPaginatedWithFilter(filter: F, page: number, limit: number): Promise<T[]>;
 }
