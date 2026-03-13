8e28632163feef0b13bc84c03d4515f62b370725
AbdullahSafwan <abdullahsafwan768@gmail.com>
2026-03-14 04:19:27 +0500

    formatting

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -2,15 +2,15 @@ import mongoose from "mongoose";
 import IUser from "../interfaces/user.interface";
 
 const userSchema = new mongoose.Schema(
-    {
-        name: { type: String, required: true},
-        email: {type: String, required: true},
-        password: {type: String, required: true},
-    },
-    {
-        timestamps: true,
-    }
-)
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const User = mongoose.model<IUser>("User", userSchema);
 
