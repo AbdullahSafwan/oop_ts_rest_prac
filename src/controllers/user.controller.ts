f34c3c5a12ccc500378b9ff931b463fa838fdfa8
AbdullahSafwan <abdullahsafwan768@gmail.com>
2026-03-15 16:43:18 +0500

    formatting

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -3,31 +3,31 @@ import IUser from "../interfaces/user.interface";
 
 import UserService from "../services/user.service";
 
-class UserController{
-    private readonly userService: UserService;
+class UserController {
+  private readonly userService: UserService;
 
-    constructor(){
-        this.userService = new UserService();
-    }
+  constructor() {
+    this.userService = new UserService();
+  }
 
-    async create(req: Request, res: Response){
-        try {
-            const data: IUser =req.body;
-            const user = await this.userService.create(data);
-            res.status(201).json(user);
-        } catch (error : unknown) {
-            throw new Error(error as string);
-        }
+  async create(req: Request, res: Response) {
+    try {
+      const data: IUser = req.body;
+      const user = await this.userService.create(data);
+      res.status(201).json(user);
+    } catch (error: unknown) {
+      throw new Error(error as string);
     }
+  }
 
-    async findAll(req: Request, res:Response){
-        try {
-            const users = await this.userService.findAll();
-            res.status(200).json(users)
-        } catch (error : unknown) {
-            throw new Error(error as string);
-        }
+  async findAll(req: Request, res: Response) {
+    try {
+      const users = await this.userService.findAll();
+      res.status(200).json(users);
+    } catch (error: unknown) {
+      throw new Error(error as string);
     }
+  }
 }
 
 export default UserController;
