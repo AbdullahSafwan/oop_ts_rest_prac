00c83965ec69651f3617c641e2ef66ddcc3560f6
AbdullahSafwan <abdullahsafwan768@gmail.com>
2026-03-15 16:50:54 +0500

    errorHandler

---
+++ src/helpers/errorHandler.ts
@@ -0,0 +1,13 @@
+import { Request, Response, NextFunction } from "express";
+
+class ErrorHandler {
+  static NotFound(req: Request, res: Response, _next: NextFunction) {
+    res.status(404).json({ message: "Resource Not Found" });
+  }
+
+  static serverError(error: Error, req: Request, res: Response, _next: NextFunction) {
+    res.status(500).json({ message: error.message });
+  }
+}
+
+export default ErrorHandler;
